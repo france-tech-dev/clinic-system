f9d69dbe0d182b8e80dfb35233e51cdb1ba82847
France Dev <france.dev@outlook.com>
2026-07-25 22:00:19 -0300

    refactor: Move getActiveOrganization function to a dedicated file for better organization and maintainability

---
+++ src/server/organizations/active-organization.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { db } from "@/shared/lib/prisma";
+
+export async function getActiveOrganization(userId: string) {
+  const memberUser = await db.member.findFirst({
+    where: {
+      userId,
+      status: "ativo",
+    },
+  });
+
+  if (!memberUser) {
+    return null;
+  }
+
+  const activeOrganization = await db.organization.findFirst({
+    where: {
+      id: memberUser.organizationId,
+    },
+  });
+  return activeOrganization;
+}
--- src/server/organizations/organizations.ts
+++ src/server/organizations/organizations.ts
@@ -19,26 +19,6 @@ export async function getOrganizations() {
   return organizations;
 }
 
-export async function getActiveOrganization(userId: string) {
-  const memberUser = await db.member.findFirst({
-    where: {
-      userId,
-      status: "ativo",
-    },
-  });
-
-  if (!memberUser) {
-    return null;
-  }
-
-  const activeOrganization = await db.organization.findFirst({
-    where: {
-      id: memberUser.organizationId,
-    },
-  });
-  return activeOrganization;
-}
-
 export async function getOrganizationBySlug(slug?: string) {
   try {
     const organizationBySlug = await db.organization.findFirst({
